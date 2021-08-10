a8e037a495c139f4d768ff91a583b438853f1c51
InnocentEdem <82113948+InnocentEdem@users.noreply.github.com>
2021-08-10 09:52:04 +0000

    Initial commit

---
+++ dogYears.js
@@ -0,0 +1,10 @@
+var myAge = 30  //this is my age
+var earlyYears = 2;
+earlyYears *=10.5;
+var laterYears =myAge-2; //accounted for already
+laterYears *= 4 // remainder of years
+myAgeInDogYears = earlyYears + laterYears;
+myName='edem'.toLowerCase()  //convert my name into lowercase
+console.log(`My name is ${myName}. I am Human age ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
